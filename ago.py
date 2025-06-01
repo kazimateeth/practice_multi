f59162ebe6b488067c039f84bdb833a1bac57f33
kazimateeth <kazama.math.chiba@gmail.com>
2025-06-01 11:28:57 +0900

    UI

--- ago.py
+++ ago.py
@@ -7,4 +7,9 @@ col1 , col2 =st.columns(2)
 with col1:
  st.image("https://cdn.pixabay.com/photo/2021/03/08/18/16/man-6079789_1280.jpg")
 with col2:
- st.write("暑かった")
+ st.write("上着：")
+ st.write("トップス：")
+ st.write("ボトムス：")
+ st.write("防寒具：")
+ st.write("評価：暑かった")
+ 
--- pages/hyouka.py
+++ pages/hyouka.py
@@ -11,23 +11,22 @@ winter_clothes = "防寒具"
 evaluate = {}
 evaluate["日付"] = today
 
-st.title(today.strftime("%Y-%m-%d")+"の評価")
-col1 , col2 , col3 , col4, col5 = st.columns(5)
-with col1:
-    option1=["ダウンジャケット","厚手の上着","薄手の上着","なし"]
-    evaluate[jacket] = st.selectbox(jacket,option1)
-with col2:
-    option2=["厚めの長袖","薄手の長袖","半袖",]
-    evaluate[tops] = st.selectbox(tops,option2)
-with col3:
-    option3=["厚手のボトムス","薄手のボトムス","短いボトムス",]
-    evaluate[bottoms] = st.selectbox(bottoms,option3)
-with col4:
-    option4=["しっかり","少し","なし",]
-    evaluate[winter_clothes] = st.selectbox(winter_clothes,option4)
-with col5:
-    st.write("気温")
-    evaluate["評価"] = st.radio("これを…", ("暑かった", "寒かった", "ちょうどいい"))
+st.title(today.strftime("%Y-%m-%d")+"の評価をする")
+
+option1=["なし","ダウンジャケット","厚手の上着","薄手の上着"]
+evaluate[jacket] = st.selectbox(jacket,option1)
+
+option2=["厚めの長袖","薄手の長袖","半袖",]
+evaluate[tops] = st.selectbox(tops,option2)
+
+option3=["厚手のボトムス","薄手のボトムス","短いボトムス",]
+evaluate[bottoms] = st.selectbox(bottoms,option3)
+
+option4=["なし","しっかり","少し",]
+evaluate[winter_clothes] = st.selectbox(winter_clothes,option4)
+
+st.write("気温")
+evaluate["評価"] = st.radio("これを…", ("暑かった", "寒かった", "ちょうどいい"))
 
 #戻り値の確認用
 if st.button("決定"):
